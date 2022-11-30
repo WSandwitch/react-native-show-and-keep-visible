2ae26be7af05e78b0cd314ff9b1cf63369aabc09
WSandwitch <whitesandwitch@yandex.com>
2022-11-30 11:54:20 +0300

    Update KCKeepAwake.java

--- android/src/main/java/com/corbt/keepawake/KCKeepAwake.java
+++ android/src/main/java/com/corbt/keepawake/KCKeepAwake.java
@@ -26,7 +26,10 @@ public class KCKeepAwake extends ReactContextBaseJavaModule {
             activity.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+                    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
                 }
             });
         }
@@ -40,7 +43,10 @@ public class KCKeepAwake extends ReactContextBaseJavaModule {
             activity.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    activity.getWindow().clearFlags(android.view.WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+                    activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
                 }
             });
         }
